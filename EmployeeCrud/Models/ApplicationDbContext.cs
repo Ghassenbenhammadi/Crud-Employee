e2c695623ac07c40cdfaaa629d56c387a7a760d0
GhassenBenhammadi <benhammdighassen@gmail.com>
2023-01-03 21:37:35 +0100

    first commit

---
+++ EmployeeCrud/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeCrud.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=DESKTOP-D2HJFH2; initial catalog=EmployeeDataBase; integrated security=SSPI;");
+        }
+    }
+}
